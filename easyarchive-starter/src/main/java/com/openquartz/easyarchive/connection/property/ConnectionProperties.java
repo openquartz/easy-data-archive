755bdd3832cb3b0bef4d97d77c9a4161501708c8
svnee <xuzhaoah@163.com>
2026-05-31 22:08:29 +0800

    feat: add pool configuration properties to ConnectionProperties

--- easyarchive-starter/src/main/java/com/openquartz/easyarchive/connection/property/ConnectionProperties.java
+++ easyarchive-starter/src/main/java/com/openquartz/easyarchive/connection/property/ConnectionProperties.java
@@ -15,24 +15,25 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class ConnectionProperties {
 
     /**
-     * 源数据库连接
-     */
-    private String source = "jdbc:mysql://localhost:3306/source_db";
-    private String sourceUsername = "root";
-    private String sourcePassword = "password";
-
-    /**
-     * 目标数据库连接
-     */
-    private String target = "jdbc:mysql://localhost:3306/target_db";
-    private String targetUsername = "root";
-    private String targetPassword = "password";
-
-    /**
      * 配置数据库连接
      */
     private String config = "jdbc:mysql://localhost:3306/config_db";
     private String configUsername = "root";
     private String configPassword = "password";
+
+    /**
+     * 连接池配置
+     */
+    private PoolProperties pool = new PoolProperties();
+
+    @Getter
+    @Setter
+    public static class PoolProperties {
+        private int maximumPoolSize = 10;
+        private int minimumIdle = 2;
+        private long connectionTimeout = 30000;
+        private long idleTimeout = 600000;
+        private long maxLifetime = 1800000;
+    }
 
 }
