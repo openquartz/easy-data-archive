0872e5ed9ab650834fd96c45423d10515b391ba5
svnee <xuzhaoah@163.com>
2026-06-08 19:29:37 +0800

    chore: finalize data permission refactor
    Mark DataPermissionServiceImpl as @Deprecated since main consumers have
    been migrated to ArchiveResourceAccessService and
    DatasourceAuthorizationService. Add Authorization section to README
    documenting the new role/capability model.

--- easyarchive-starter/src/main/java/com/openquartz/easyarchive/starter/service/impl/DataPermissionServiceImpl.java
+++ easyarchive-starter/src/main/java/com/openquartz/easyarchive/starter/service/impl/DataPermissionServiceImpl.java
@@ -21,6 +21,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Deprecated
 @Service
 @RequiredArgsConstructor
 public class DataPermissionServiceImpl implements DataPermissionService {
