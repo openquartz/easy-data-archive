93950848e667769e67cc573451dcd2b6ff0be10d
svnee <xuzhaoah@163.com>
2026-06-09 17:28:30 +0800

    refactor: use RecentTaskVO in overview to format datetime

--- easyarchive-starter/src/main/java/com/openquartz/easyarchive/starter/model/dto/ArchiveGroupOverviewView.java
+++ easyarchive-starter/src/main/java/com/openquartz/easyarchive/starter/model/dto/ArchiveGroupOverviewView.java
@@ -1,6 +1,5 @@
 package com.openquartz.easyarchive.starter.model.dto;
 
-import com.openquartz.easyarchive.core.rule.entity.ArchiveGroupExecuteTask;
 import lombok.Data;
 
 import java.util.List;
@@ -11,5 +10,5 @@ public class ArchiveGroupOverviewView {
     private ArchiveGroupView group;
     private ArchiveGroupItemStatsView itemStats;
     private ArchiveGroupTaskStatsView taskStats;
-    private List<ArchiveGroupExecuteTask> recentTasks;
+    private List<RecentTaskVO> recentTasks;
 }
